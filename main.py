d8c2db62b28bed1db507769376ed0736bc54bfe6
Hamza <h.saadi2021@gmail.com>
2023-09-15 23:49:11 +0300

    changes

--- main.py
+++ main.py
@@ -50,9 +50,10 @@ def start(best_buy):
 def main():
     product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
                     products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                    products.Product("Google Pixel 7", price=500, quantity=250)
+                    products.Product("Google Pixel 7", price=500, quantity=250),
+                    products.NonStockedProduct("Windows License", price=125),
+                    products.LimitedProduct("Shipping", price=10, quantity=250, max_purchase=1)
                     ]
-
     best_buy = store.Store(product_list)
     start(best_buy)
 
--- products.py
+++ products.py
@@ -1,3 +1,43 @@
+from abc import ABC, abstractmethod
+
+# Abstract Promotion class
+class Promotion(ABC):
+
+    def __init__(self, name):
+        self.name = name
+
+    @abstractmethod
+    def apply_promotion(self, product, quantity):
+        pass
+
+# Concrete Promotion classes
+class PercentageDiscountPromotion(Promotion):
+
+    def __init__(self, percentage):
+        super().__init__("Percentage Discount")
+        self.percentage = percentage
+
+    def apply_promotion(self, product, quantity):
+        return product.get_price() * quantity * (1 - (self.percentage / 100))
+
+class SecondItemAtHalfPricePromotion(Promotion):
+
+    def __init__(self):
+        super().__init__("Second Item at Half Price")
+
+    def apply_promotion(self, product, quantity):
+        full_price_items = quantity // 2 + quantity % 2
+        half_price_items = quantity // 2
+        return product.get_price() * (full_price_items + 0.5 * half_price_items)
+
+class BuyTwoGetOneFreePromotion(Promotion):
+
+    def __init__(self):
+        super().__init__("Buy 2 Get 1 Free")
+
+    def apply_promotion(self, product, quantity):
+        payable_quantity = 2 * (quantity // 3) + quantity % 3
+        return product.get_price() * payable_quantity
 class Product:
     def __init__(self, name, price, quantity):
         if not name:
@@ -9,6 +49,7 @@ class Product:
         self.name = name
         self.price = price
         self.quantity = quantity
+        self.promotion = None
 
     def get_price(self):
         return self.price
@@ -16,14 +57,23 @@ class Product:
     def get_quantity(self):
         return self.quantity
 
+    def set_promotion(self, promotion):
+        self.promotion = promotion
+
     def buy(self, quantity):
         if quantity > self.quantity:
             raise Exception("Not enough stock available.")
+        if self.promotion:
+            total_price = self.promotion.apply_promotion(self, quantity)
+        else:
+            total_price = self.price * quantity
         self.quantity -= quantity
         if self.quantity == 0:
             self.is_active = False
-        return self.price * quantity, f"{quantity} unit(s) of {self.name} purchased."
-
+        return total_price , f"{quantity} unit(s) of {self.name} purchased."
+    def show(self):
+        promo_str = f"Current Promotion: {self.promotion.name}" if self.promotion else "No current promotion"
+        return f"{self.name}: Price: {self.price}, Quantity: {self.quantity}. {promo_str}"
 
 class NonStockedProduct(Product):
     def __init__(self, name, price):
