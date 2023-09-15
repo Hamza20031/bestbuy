85c4964ed55f04cb8aa797a4ca01b78e26d4ee0a
Hamza <h.saadi2021@gmail.com>
2023-09-15 23:37:21 +0300

    changes

--- products.py
+++ products.py
@@ -22,4 +22,32 @@ class Product:
         self.quantity -= quantity
         if self.quantity == 0:
             self.is_active = False
-        return self.price * quantity , f"{quantity} unit(s) of {self.name} purchased."
+        return self.price * quantity, f"{quantity} unit(s) of {self.name} purchased."
+
+
+class NonStockedProduct(Product):
+    def __init__(self, name, price):
+        super().__init__(name, price, 0)  # Quantity set to zero
+
+    def buy(self, quantity):
+        if quantity > 1:
+            raise Exception("Non-stocked products can only be purchased one at a time.")
+        return super().buy(quantity)
+
+    def show(self):
+        return f"{super().show()} (Non-Stocked Product)"
+
+
+# For Limited Products
+class LimitedProduct(Product):
+    def __init__(self, name, price, quantity, max_purchase):
+        super().__init__(name, price, quantity)
+        self.max_purchase = max_purchase
+
+    def buy(self, quantity):
+        if quantity > self.max_purchase:
+            raise Exception(f"Cannot buy more than {self.max_purchase} of this product at a time.")
+        return super().buy(quantity)
+
+    def show(self):
+        return f"{super().show()} (Limited Product: Max Purchase {self.max_purchase})"
